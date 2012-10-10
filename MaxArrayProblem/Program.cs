5327e3da3456b341816cc29d7e2658ce10df19cc
Rob Gibson <gibsonr@gmail.com>
2012-10-09 18:34:49 -0700

    Fixed it and made it a little cleaner

--- MaxArrayProblem/Program.cs
+++ MaxArrayProblem/Program.cs
@@ -2,15 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace MaxArrayProblem
 {
     static class Program
     {
-        static int executions1 = 0;
-        static int executions2 = 0;
-        static int executions3 = 0;
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -20,24 +16,55 @@ namespace MaxArrayProblem
             int[] arr2 = { -18, -47, -40, -43, -2, 48, 31, -24, 36, -49, 4, -29, -4, -39, 12, 24, 8, 40, 45, -17, 6, -11, -5, -30, -8, 25, -44, -9, -20, -50, -12, -32, 41, 10, -42, -15, 11, -38, 37, 21, 33, -22, 16, -41, -46, -33, -26, 7, -3, -28, 29, 20, 27, 3, 15, 49, 23, -1, 14, 32, -31, -13, -48, -14, 13, 39, 46, -35, -36, 0, 17, -27, -21, 28, -7, 44, -10, 34, -19, 47, 42, -34, 5, 26, -45, 35, 9, -25, 38, -37, -23, 22, -6, -16, 18, 43, 30, 2, 19, 1 }; // 322
             int[] arr3 = { 3, 35, -45, 12, 44, 6, 46, -33, -15, 39, -19, 19, -31, -41, -35, 23, -25, 42, 2, 22, 40, -40, -24, 38, 14, 10, -44, 31, 16, 48, 29, 20, 32, -13, 43, -49, -10, 21, 28, 49, -28, -36, 36, -21, -26, 15, 25, -34, -20, -42, -43, 33, 1, -39, 45, 18, 27, -12, -22, -1, 47, -17, -4, 41, -32, -2, -5, -48, -7, 37, 8, -3, 26, -27, -6, 7, -47, -8, 11, -46, 9, -23, 4, -30, -9, -18, -29, 17, -11, 30, -50, -14, 24, 5, 34, 0, 13, -37, -16, -38 }; // 305
 
-            for (int i = 0; i < arr1.Length; ++i)
-            {
-                Console.Write("{0}({2})\t{1}", arr1[i], (i + 1) % 10 == 0 ? "\n" : "", i);
-            }
-            Console.WriteLine();
-
-            Console.WriteLine("Algorithm 1 Result: {0}", MaxSubarray1(arr2));
-            Console.ReadKey();
-
-            Console.WriteLine("Algorithm 2 Result: {0}", MaxSubarray2(arr2));
-            Console.ReadKey();
-
-            Console.WriteLine("Algorithm 3 Result: {0}", MaxSubarray3(arr2, 0, arr1.Length - 1));
+            
+            CalculateArray(arr3);
             Console.ReadKey();
 
         }
 
-        static int MaxSubarray1(int[] arr)
+        static void CalculateArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; ++i)
+            {
+                Console.Write("{0}({2})\t{1}", arr[i], (i + 1) % 10 == 0 ? "\n" : "", i);
+            }
+            Console.WriteLine();
+
+
+
+            Console.WriteLine("Algorithm 1 Result: {0}", MaxSubArray1(arr));
+            Console.WriteLine("Algorithm 2 Result: {0}", MaxSubArray2(arr));
+            Console.WriteLine("Algorithm 3 Result: {0}", MaxSubArray3(arr, 0, arr.Length - 1));
+            Console.WriteLine("Algorithm 4 Result: {0}", MaxSubArray4(arr));
+        }
+
+        static int MaxSubArray1(int[] arr)
+        {
+            int max = 0;
+            for (int i = 0; i < arr.Length; ++i)
+            {
+                int maxForCurrRange = 0;
+                for (int j = i; j < arr.Length; ++j)
+                {
+                    int currSum = 0;
+                    for (int k = i; k <= j; ++k)
+                    {
+                        currSum += arr[k];
+                    }
+                    if (currSum > maxForCurrRange)
+                    {
+                        maxForCurrRange = currSum;
+                    }
+                }
+                if (max < maxForCurrRange)
+                {
+                    max = maxForCurrRange;
+                }
+            }
+            return max;
+        }
+
+        static int MaxSubArray2(int[] arr)
         {
             int max = 0;
 
@@ -54,14 +81,57 @@ namespace MaxArrayProblem
                         max = currentMax;
                     }
                 }
-
-                
             }
 
             return max;            
         }
 
-        static int MaxSubarray2(int[] arr)
+        
+
+        static int MaxSubArray3(int[] arr, int start, int finish)
+        {
+            
+            if (start == finish) {
+                return arr[start];
+            }
+            else {
+                int leftSum = MaxSubArray3(arr, start, (start + finish) / 2);
+                int rightSum = MaxSubArray3(arr, (start + finish) / 2 + 1, finish);
+
+                int midSum = 0;
+                int currLeftSum = 0;
+                int currRightSum = 0;
+                int bestLeftSum = 0;
+                int bestRightSum = 0;
+
+                for (int i = (start+finish) / 2; i >= start; --i)
+                {
+                    currLeftSum += arr[i];
+
+                    if (currLeftSum > bestLeftSum)
+                    {
+                        bestLeftSum = currLeftSum;
+                    }
+                    
+                }
+                for (int i = (start+finish) / 2 + 1; i <= finish; ++i)
+                {
+                    currRightSum += arr[i];
+                
+                    if (currRightSum > bestRightSum)
+                    {
+                        bestRightSum = currRightSum;
+                    }
+                }
+
+                midSum = bestRightSum + bestLeftSum;
+
+                return Math.Max(midSum, Math.Max(leftSum, rightSum));
+
+            }
+        }
+
+        static int MaxSubArray4(int[] arr)
         {
             int currentMax = 0;
             int endHere = 0;
@@ -84,69 +154,6 @@ namespace MaxArrayProblem
             }
 
             return currentMax;
-        }
-
-        static int MaxSubarray3(int[] arr, int start, int finish)
-        {
-            
-            if (start == finish) {
-                return arr[start];
-            }
-            else {
-                int leftSum = MaxSubarray3(arr, start, (start + finish) / 2);
-                int rightSum = MaxSubarray3(arr, (start + finish) / 2 + 1, finish);
-
-                int midSum = 0;
-                int currLeftSum = 0;
-                int currRightSum = 0;
-                int bestLeftSum = 0;
-                int bestRightSum = 0;
-                int bestLeft = 0;
-                int bestRight = 0;
-
-                for (int i = (start+finish) / 2; i >= start; --i)
-                {
-                    currLeftSum += arr[i];
-
-                    if (arr[i] + bestLeft < 0)
-                    {
-                        break;
-                    }
-                    else if (arr[i] > bestLeft)
-                    {
-                        bestLeft = arr[i];
-                    }
-
-                    if (currLeftSum > bestLeftSum)
-                    {
-                        bestLeftSum = currLeftSum;
-                    }
-                    
-                }
-                for (int i = (start+finish) / 2 + 1; i <= finish; ++i)
-                {
-                    currRightSum += arr[i];
-                    if (arr[i] + bestRight < 0)
-                    {
-                        break;
-                    }
-                    else if (arr[i] > bestRight) 
-                    {
-                        bestRight = arr[i];
-                    }
-
-                    if (currRightSum > bestRightSum)
-                    {
-                        bestRightSum = currRightSum;
-                    }
-                }
-
-                midSum = bestRightSum + bestLeftSum;
-
-                return Math.Max(midSum, Math.Max(leftSum, rightSum));
-
-            }
-
         }
     }
 }
